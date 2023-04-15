6851be234cbe8bad7242948d96c1fe3d1a6947e0
Kevin <itsksrof@gmail.com>
2023-04-15 19:42:46 +0200

    feat: added String method to allow easy debugging, plus unit test

--- ast/ast.go
+++ ast/ast.go
@@ -1,9 +1,13 @@
 package ast
 
-import "go-interpreter/token"
+import (
+	"bytes"
+	"go-interpreter/token"
+)
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -32,6 +36,19 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+// String creates a buffer and writes the return value of each
+// statement's String() method to it. And then it returns the buffer
+// as a string.
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
 // LetStatement has 'Name' to hold the identifiers of the binding
 // and 'Value' for the expression that produces the value. The two
 // methods statementNode and TokenLiteral satisfy the Statement and
@@ -44,6 +61,20 @@ type LetStatement struct {
 
 func (ls *LetStatement) statementNode() {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+	return out.String()
+}
 
 // Identifier represents the name of a binded variable for later reuse.
 type Identifier struct {
@@ -53,6 +84,7 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode() {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string { return i.Value }
 
 // ReturnStatement represents solely the keyword return and an expression.
 type ReturnStatement struct {
@@ -62,3 +94,31 @@ type ReturnStatement struct {
 
 func (rs *ReturnStatement) statementNode() {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+	return out.String()
+}
+
+// ExpressionStatement it's only a wrapper that consists solely of one expression.
+type ExpressionStatement struct {
+	Token		token.Token // the first token of the expression
+	Expression	Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}	
+
+	return ""
+}
