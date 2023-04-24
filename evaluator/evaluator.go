892700e395aaf5aa1c3bfebbfe33415049e8c083
Kevin <itsksrof@gmail.com>
2023-04-24 08:56:16 +0200

    feat: added support for string concatenation

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -241,10 +241,15 @@ func evalMinusOperatorExpression(right object.Object) object.Object {
 
 // evalInfixExpression checks whether or not the left and right operands are integers.
 // If they are calls evalIntegerInfixExpression, otherwise returns NULL.
+
+// evalInfixExpression checks object types and operators and uses them to determine
+// the eval function that needs to be called.
 func evalInfixExpression(operator string, left, right object.Object) object.Object {
 	switch {
 	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
 		return evalIntegerInfixExpression(operator, left, right)
+	case left.Type() == object.STRING_OBJ && right.Type() == object.STRING_OBJ:
+		return evalStringInfixExpression(operator, left, right)
 	case operator == "==":
 		return nativeBoolToBooleanObject(left == right)
 	case operator == "!=":
@@ -284,6 +289,18 @@ func evalIntegerInfixExpression(operator string, left, right object.Object) obje
 	default:
 		return newError("unknown operator: %s %s %s", left.Type(), operator, right.Type())
 	}
+}
+
+// evalStringInfixExpression unwraps the string objects and constructs a new string by
+// concatenating both operands.
+func evalStringInfixExpression(operator string, left, right object.Object) object.Object {
+	if operator != "+" {
+		return newError("unknown operator: %s %s %s", left.Type(), operator, right.Type())
+	}
+
+	leftVal := left.(*object.String).Value
+	rightVal := right.(*object.String).Value
+	return &object.String{Value: leftVal + rightVal}
 }
 
 // evalIfExpression first evaluates the condition contained inside the if-expression.
