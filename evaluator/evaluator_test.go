494d6d52ad276bbbbee1408e75281e6f3592b4ae
Kevin <ksrof@proton.me>
2023-05-06 14:07:56 +0200

    feat: added built-in function first to get the first element of an array

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -299,6 +299,10 @@ func TestBuiltinFunctions(t *testing.T) {
 		{`len("hello world")`, 11},
 		{`len(1)`, "argument to `len` not supported. got INTEGER"},
 		{`len("one", "two")`, "wrong number of arguments. got=2, want=1"},
+		{`first([1,2,3])`, 1},
+		{`first()`, "wrong number of arguments. got=0, want=1"},
+		{`first("array")`, "argument to 'first' must be ARRAY, got=STRING"},
+		{`first([])`, nil},
 	}
 
 	for _, tt := range tests {
